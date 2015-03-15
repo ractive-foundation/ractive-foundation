c4c6ea497f51ba9ce9f3c9ed6699bb96b075b6fa
Pavel Shum <pv.shum@gmail.com>
2015-03-15 13:09:34 +1100

    Adjusting gulpfile to include timplates and components compilers

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,9 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    connect = require('gulp-connect');
-
-gulp.task('sass', function () {
-    gulp.src('./src/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./public/css'));
-    gulp.src('./node_modules/foundation/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./public/css/foundation'));
-});
-
-gulp.task('concat-components', function() {
-    return gulp.src('./src/components/**/*.js')
-        .pipe(concat('components.js'))
-        .pipe(gulp.dest('./public/js'));
-});
+    connect = require('gulp-connect'),
+    ractiveParse = require('./tasks/ractiveParse.js'),
+    ractiveConcatComponents = require('./tasks/ractiveConcatComponents.js');
 
 gulp.task('connect', function() {
     connect.server({
@@ -35,15 +22,45 @@ gulp.task('copy-ractive', function () {
         .pipe(gulp.dest('./public/js'));
 });
 
+
+gulp.task('build-sass', function () {
+    gulp.src('./src/**/*.scss')
+        .pipe(sass())
+        .pipe(concat('components.css'))
+        .pipe(gulp.dest('./public/css'));
+
+    gulp.src('./node_modules/foundation/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css/foundation'));
+});
+
+gulp.task('ractive-build-templates', function() {
+    return gulp.src('./src/components/**/*.hbs')
+        .pipe(ractiveParse())
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('ractive-build-components', function() {
+    return gulp.src('./src/components/**/*.js')
+        .pipe(ractiveConcatComponents())
+        .pipe(concat('components.js'))
+        .pipe(gulp.dest('./public/js/'));
+});
+
+
 gulp.task('watch', function () {
     gulp.watch(
         [
             './public/*.html',
-            './src/**/*.js'
+            './src/**/*.js',
+            './src/**/*.hbs',
+            './src/**/*.sass'
         ],
         [
-            'sass',
-            'concat-components',
+            'build-sass',
+            'ractive-build-templates',
+            'ractive-build-components',
             'copy-ractive',
             'html'
         ]
