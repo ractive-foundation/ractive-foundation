9f14ea551ac6dc067868e80961ca11cedaf4287a
Scott Maclure <scott@maclure.com.au>
2015-03-16 17:49:37 +1100

    Added uxaccordion support, refactored some components to deal with nested value changes.

--- src/components/uxaccordion/uxaccordion.js
+++ src/components/uxaccordion/uxaccordion.js
@@ -1,3 +1,11 @@
 Ractive.extend({
-    template: RactiveF.templates.uxaccordion
+    template: RactiveF.templates.uxaccordion,
+	oninit: function () {
+		this.on('*.changeAccordion', function (syntheticEvent) {
+			_.map(this.findAllComponents('uxaccordionitem'), function (component) {
+				var isActive = component.get('id') === syntheticEvent.context.id;
+				component.set('active', isActive);
+			});
+		});
+	}
 });
