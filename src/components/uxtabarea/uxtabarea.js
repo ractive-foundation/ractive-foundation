c25c6e79a7ff92c369902c23b6b4b9e75e2cf2ba
Nathan Flew <nathan.flew@optus.com.au>
2015-03-19 20:12:25 +1100

    Only deal with DIRECT children on the current tab area. Let nested areas look after themselves also.

--- src/components/uxtabarea/uxtabarea.js
+++ src/components/uxtabarea/uxtabarea.js
@@ -2,20 +2,21 @@ Ractive.extend({
 	template: RactiveF.templates.uxtabarea,
 	oninit: function () {
 
-		var tabLinks = this.findAllComponents('uxtablink');
-		var tabPanes = this.findAllComponents('uxtabpane');
+		var tabLinks = this.findComponent('uxtablinks');
+		var tabPanes = this.findComponent('uxtabpanes');
 
-		_.each(tabLinks, function (tabLink) {
-			var childPane = _.filter(tabPanes, function (tabPane) {
-				return tabLink.get('controls') === tabPane.get('id');
-			});
+		var tabLink = tabLinks.findAllChildComponents('uxtablink');
+		var tabPane = tabPanes.findAllChildComponents('uxtabpane');
 
-			if (childPane.length) {
-				tabLink.set('tabPane', childPane[0]);
+		_.each(tabLink, function (link, i) {
+			var childPane;
+			if (childPane = tabPane[i]) {
+				childPane.set('uidLink', link._guid);
+				link.set({
+					tabPane: childPane,
+					uid: link._guid
+				});
 			}
 		});
-
-
-
 	}
 });
