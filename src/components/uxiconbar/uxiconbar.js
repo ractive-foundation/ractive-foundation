e22ca8a80f0dac47ff770c4a5c2e8aa8ae169cb2
Scott Maclure <scott.maclure1@optus.com.au>
2015-03-19 17:30:18 +1100

    Added numberToWord functionality.

--- src/components/uxiconbar/uxiconbar.js
+++ src/components/uxiconbar/uxiconbar.js
@@ -1,3 +1,35 @@
 Ractive.extend({
-	template: RactiveF.templates.uxiconbar
+	template: RactiveF.templates.uxiconbar,
+	data: {
+		/**
+		 * TODO Move to generic helpers location?
+		 * @param num A number, e.g. 1
+		 * @returns {string} The number as a word, e.g. "one", "three", etc.
+		 */
+		numberToWord: function (num) {
+
+			var supportedWords = [
+				'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'
+			];
+
+			if (!supportedWords[num]) {
+				console.error('uxiconbar#numberToWord: num NOT supported: ' + num);
+				return '';
+			}
+
+			return supportedWords[num];
+
+		}
+	},
+	oninit: function () {
+
+		var childCount = this.findAllComponents('uxiconbaritem').length;
+
+		if (childCount < 1 || childCount > 8) {
+			console.error('uxiconbar only supports between 1-8 items.');
+		}
+
+		this.set('upNum', childCount);
+
+	}
 });
