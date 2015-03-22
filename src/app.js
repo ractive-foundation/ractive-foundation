edb776aefc59ba659f924723828385e253bf3ceb
Pavel Shum <pv.shum@gmail.com>
2015-03-23 09:22:08 +1100

    Fixing merge conflict

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ RactiveF = {
 	templates: {},
 	widgets: [],
 	initInstance: function (container) {
+		_.mixin(Ractive.prototype, {
+			/*
+			 * When working with nested components we only want to find child
+			 * components, not all decendants.
+			 * @param name
+			 */
+			findAllChildComponents: function (name) {
+				return _.filter(this.findAllComponents(name), function (component) {
+					return this._guid === component.parent._guid;
+				}.bind(this));
+			}
+		});
 		return new Ractive({
 			el: container,
 			template: Ractive.parse(container.innerHTML),
@@ -21,29 +33,6 @@ if (typeof document !== 'undefined') {
 		for (var i = 0; i < containers.length; i++) {
 			var instance = RactiveF.initInstance(containers[i]);
 			RactiveF.widgets.push(instance);
-var initInstance = function (i, container) {
-
-	_.mixin(Ractive.prototype, {
-		/*
-		 * When working with nested components we only want to find child
-		 * components, not all decendants.
-		 * @param name
-		 */
-		findAllChildComponents: function (name) {
-			return _.filter(this.findAllComponents(name), function (component) {
-				return this._guid === component.parent._guid;
-			}.bind(this));
-		}
-	});
-
-	return new Ractive({
-		el: container,
-		template: Ractive.parse(container.innerHTML),
-		components: RactiveF.components,
-		onrender: function () {
-			$(this.el)
-				.removeClass('hide')
-				.addClass('initialized');
 		}
 	});
 }
