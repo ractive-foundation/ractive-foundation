e19cc00eb0a284b19c9ec3659da3e80109511ca3
Scott Maclure <scott.maclure1@optus.com.au>
2015-03-23 18:29:13 +1100

    #46 Updated changeAccordion proxy event handler to use  for current accordion item, to match Foundationv5.

--- src/components/ux-accordion/ux-accordion.js
+++ src/components/ux-accordion/ux-accordion.js
@@ -1,5 +1,7 @@
 Ractive.extend({
+
 	template: RactiveF.templates['ux-accordion'],
+
 	oninit: function () {
 
 		this.set('items', this.findAllChildComponents('ux-accordionitem'));
@@ -7,8 +9,21 @@ Ractive.extend({
 		this.on('*.changeAccordion', function (syntheticEvent) {
 
 			_.each(this.get('items'), function (component) {
-				var isActive = component.get('id') === syntheticEvent.context.id;
-				component.set('active', isActive);
+
+				var userSelectedThisItem = component.get('id') === syntheticEvent.context.id;
+
+				if (userSelectedThisItem) {
+
+					// Support open and close behaviours with repeated clicking by User.
+					component.toggle('active');
+					
+				} else {
+
+					// Not where the User clicked, so close it (if open).
+					component.set('active', false);
+
+				}
+
 			});
 
 			// Stop bubbling.
