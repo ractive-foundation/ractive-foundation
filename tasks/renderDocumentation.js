6aff82752deff9ed01ae1e6b5388ae346ff0c059
Pavel Shum <pv.shum@gmail.com>
2015-03-23 19:14:39 +1100

    Refactor code to use json notation for rendering page html; Perhaps refactor later into proper templating solution e.g. Ractive js

--- tasks/renderDocumentation.js
+++ tasks/renderDocumentation.js
@@ -1,70 +1,89 @@
 var through = require('through2'),
-    gulputil = require('gulp-util'),
-    Ractive = require('ractive'),
-    marked = require('marked'),
-    fs = require('fs'),
-    find = require('find'),
-    _ = require('lodash'),
-    xml = require('xml-escape'),
-    path = require('path');
+	gulputil = require('gulp-util'),
+	Ractive = require('ractive'),
+	marked = require('marked'),
+	fs = require('fs'),
+	find = require('find'),
+	_ = require('lodash'),
+	path = require('path'),
+	makeHTML = require('json2htmljson2css').makeHTML;
 
 var PluginError = gulputil.PluginError;
 
 const PLUGIN_NAME = 'gulp-concat-documentation';
 
-function renderDocumentation(options) {
-    var stream = through.obj(function (file, enc, callback) {
-        if (file.isStream()) {
-            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
-            return callback();
-        }
+function renderDocumentation() {
+	var stream = through.obj(function (file, enc, callback) {
 
-        var pathComponents = file.history[0].split(path.sep);
-        var componentName = pathComponents.slice(-2)[0];
-        var directory = pathComponents.slice(0,-1).join('/') + '/use_cases/';
+		if (file.isStream()) {
+			this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+			return callback();
+		}
 
-        var doco = '<h1>' + componentName + '</h1>\n<br>\n';
-        var doco = doco + marked(String(file.contents));
+		try {
 
-        try {
-            // iterate over all use cases for the component
-            _.forEach(find.fileSync(/.*\.json/, directory), function(usecase) {
+			var pathComponents = file.history[0].split(path.sep),
+				componentName = pathComponents.slice(-2)[0],
+				directory = pathComponents.slice(0, -1);
 
-                var json = JSON.parse(fs.readFileSync(usecase));
+			directory.push('use_cases');
+			directory = directory.join(path.sep);
 
-                doco = doco + '<h2>Use case: ' + json.title + '</h2>';
+			var doco = makeHTML([
+				{
+					tag: 'h1',
+					content: componentName
+				}, {
+					tag: 'br'
+				}
+			]);
 
-                // iterate over all the keys in the use case data to render button
-                doco = doco + '<' + componentName;
-                _.forEach(json.data, function(value, key) {
-                    doco = doco + ' ' + key + '=\'' + value + '\'';
-                });
+			doco += marked(String(file.contents));
 
-                doco = doco + '></' + componentName + '>';
+			// iterate over all use cases for the component
+			doco += _.map(find.fileSync(/.*\.json/, directory), function (usecase) {
 
-                // iterate over all keys in the use case data to render code for this use case
-                doco = doco + '<pre><code>&lt;' + componentName;
-                _.forEach(json.data, function(value, key) {
-                    doco = doco + ' ' + xml(key) + '=\'' + xml(value) + '\'';
-                });
+				var json = JSON.parse(fs.readFileSync(usecase));
 
-                doco = doco + '&gt;&lt;/' + componentName + '&gt;</code></pre>';
+				var componentObj = {
+					// rendering component props. key="value"
+					tag: componentName,
+					attr: _.zipObject(_.keys(json.data), _.values(json.data)),
+					content: ''
+				};
 
-            });
+				return makeHTML([
+					{
+						tag: 'h2',
+						content: 'Use case: ' + json.title
+					},
+					componentObj,
+					{
+						tag: 'pre',
+						content: [{
+							tag: 'code',
+							content:  _.escape(makeHTML([componentObj]))
+						}]
+					}
+				]);
 
-            file.contents = new Buffer(doco);
-            this.push(file);
-        }
-        catch (e) {
-            console.warn('Error caught: ' + e);
-            this.push(file);
-            return callback();
-        }
+			}).join('');
 
-        callback();
-    });
+			file.contents = new Buffer(doco);
 
-    return stream;
+			this.push(file);
+		}
+
+		catch (e) {
+			console.warn('Error caught: ' + e);
+			this.push(file);
+			return callback();
+		}
+
+		callback();
+	});
+
+	return stream;
 }
 
 
