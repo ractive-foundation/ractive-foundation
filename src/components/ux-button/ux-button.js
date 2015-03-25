7b7860b2a9c56e0196f745da9d3ad3e01023f6a0
Pavel Shum <pv.shum@gmail.com>
2015-03-25 16:49:37 +1100

    Adding clickHandler (got lost in merge)

--- src/components/ux-button/ux-button.js
+++ src/components/ux-button/ux-button.js
@@ -1,3 +1,17 @@
 Ractive.extend({
-	template: RactiveF.templates['ux-button']
+	template: RactiveF.templates['ux-button'],
+	clickHandler: function () {
+
+		// if a click event is specified propagate the click event
+
+		console.log('Button event');
+
+		if (this.get('onclick')) {
+			console.log('Firing event');
+			this.fire(this.get('onclick'));
+		}
+
+		// prevent bubbling
+		return true;
+	}
 });
