2585f9fa7a82fc2c315e882417ec8c28d49bceec
Pavel Shum <pv.shum@gmail.com>
2015-03-26 12:12:56 +1100

    Filtering out ractive lifecycle events to not pollute log output

--- src/app.js
+++ src/app.js
@@ -34,10 +34,34 @@ RactiveF = {
 
 if (typeof document !== 'undefined') {
 	document.addEventListener('DOMContentLoaded', function () {
+
+		var codeBlocks = document.querySelectorAll('pre code');
+		_.each(codeBlocks, function (block) {
+			block.innerHTML = _.escape(block.innerHTML);
+			hljs.highlightBlock(block);
+		});
+
+		var handler = function (origin) {
+
+			var reservedEventNames = /^(?:change|complete|reset|teardown|update|construct|config|init|render|unrender|detach|insert)$/;
+			if (!reservedEventNames.test(this.event.name)) {
+				console.log('Event',  this.event.name);
+				console.log('Event handler arguments',  origin);
+
+				var eventName = 'events.' + origin.get('uid');
+				if (!this.get(eventName)) {
+					this.set(eventName, []);
+				}
+				this.push(eventName, this.event.name);
+			}
+		};
+
 		var containers = document.querySelectorAll('.ractivef');
 		for (var i = 0; i < containers.length; i++) {
 			var instance = RactiveF.initInstance(containers[i]);
+			instance.on('*.*', handler);
 			RactiveF.widgets.push(instance);
 		}
+
 	});
 }
