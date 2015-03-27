03bf087b3b7a44aaceda9ec4bd0d5bebcef9c92e
Nathan Flew <nathan.flew@optus.com.au>
2015-03-27 14:56:45 +1100

    #45 ux-button tests

---
+++ src/components/ux-button/ux-button.steps.js
@@ -0,0 +1,48 @@
+module.exports = function () {
+
+	// Load standard world object to be 'this' in steps.
+	this.World = require('../../world').World;
+
+	this.Before(function (callback) {
+		this.component = {};
+		this.component.container = '#childComponent ';
+
+		this.component.button = {
+			attribs: {
+				role: 'role',
+				className: 'class',
+				ariaLabel: 'aria-label',
+				tabindex: 'tabindex'
+			}
+		};
+
+		callback();
+	});
+
+	this.Given(/^I have loaded component "([^"]*)" with use case "([^"]*)"$/,
+		function (componentName, useCase, callback) {
+		this.client.loadComponentWithUseCase(componentName, useCase, callback);
+	});
+
+	this.Then(/^the element "([^"]*)" should be displayed$/, function (selector, callback) {
+		var self = this;
+		this.client.isExisting(this.component.container + selector, function (err, isExisting) {
+			self.assert(isExisting);
+			callback();
+		});
+	});
+
+	this.Then(/^the element "([^"]*)" should have the "([^"]*)" of "([^"]*)"$/,
+		function (selector, attribName, attribValue, callback) {
+		var self = this;
+		this.client.getAttribute(this.component.container + selector, this.component.button.attribs[attribName],
+			function (err, attr) {
+				self.assert.equal(attr, attribValue);
+				callback();
+			});
+	});
+
+
+
+
+};
