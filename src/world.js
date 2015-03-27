509e46ee07a0b0ad34f2b73cd13085378cb4b4cb
Nathan Flew <nathan.flew@optus.com.au>
2015-03-27 14:55:35 +1100

    #45 Clean up world.js and move out common functions

--- src/world.js
+++ src/world.js
@@ -1,8 +1,9 @@
 /**
  * Shared environment for all tests.
  */
+
+// @see http://webdriver.io/api.html
 var webdriverio = require('webdriverio');
-var selenium = require('selenium-standalone');
 
 // @see http://nodejs.org/api/assert.html
 // Assertion library of choice.
@@ -10,13 +11,11 @@ var assert = require('assert');
 
 // WebdriverIO Browser choices.
 const BROWSER_PHANTOMJS = 'phantomjs';
-const BROWSER_CHROME = 'chrome';
-const BROWSER_FIREFOX = 'firefox';
 
 const WEBDRIVER_TIMEOUT = 5000;
 
 // TODO The port will inevitably need to be dynamic.
-var COMPONENT_BASE_PATH = 'http://localhost:9080/test.html#!/component/';
+var COMPONENT_BASE_PATH = 'http://localhost:9080/testRunner.html#!/component/$1/use-case/$2';
 
 var WorldConstructor = function WorldConstructor(callback) {
 
@@ -25,19 +24,6 @@ var WorldConstructor = function WorldConstructor(callback) {
 			browserName: BROWSER_PHANTOMJS
 		}
 	};
-
-	// Start webdriver server.
-	// TODO we need to move this out from here. No need to start this server everytime. Only once.
-	selenium.start({ stdio: 'pipe' }, function (err, child) {
-		if (err) {
-			throw new Error(err);
-		}
-		// Purely selenium server debugging output.
-		//child.stderr.on('data', function (data){
-		//	console.log(data.toString());
-		//});
-	});
-
 
 	var client = webdriverio.remote(options).init();
 
@@ -55,8 +41,9 @@ var WorldConstructor = function WorldConstructor(callback) {
 	 * Common functions that will be used in the suite.
 	 */
 
-	client.addCommand('loadComponent', function(componentName, callback) {
-		this.url(COMPONENT_BASE_PATH + componentName, callback);
+	client.addCommand('loadComponentWithUseCase', function(componentName, useCase, callback) {
+		var url = COMPONENT_BASE_PATH.replace('$1', componentName).replace('$2', useCase);
+		this.url(url, callback);
 	});
 
 	callback(world);
