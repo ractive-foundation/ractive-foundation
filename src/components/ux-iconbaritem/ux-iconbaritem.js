a103729ef32c383237158ffd1d6cd808bf15cf6e
Scott Maclure <scott@maclure.com.au>
2015-04-07 20:49:13 +1000

    [#77] Make child component aware of changes to 'datamodel' and then map back to root-level data.

--- src/components/ux-iconbaritem/ux-iconbaritem.js
+++ src/components/ux-iconbaritem/ux-iconbaritem.js
@@ -1,10 +1,23 @@
 Ractive.extend({
+
 	template: RactiveF.templates['ux-iconbaritem'],
-	components: RactiveF.components,
-	isolated: true,
+
 	computed: {
 		guid: function () {
 			return this._guid;
 		}
+	},
+
+	onrender: function () {
+
+		// Wait for parent component to set "datamodel" and then map that back into data again.
+		this.observe('datamodel', function (newDataModel) {
+			if (newDataModel) {
+				// Lift datamodel data into root data scope.
+				this.set(newDataModel);
+			}
+		});
+
 	}
+
 });
