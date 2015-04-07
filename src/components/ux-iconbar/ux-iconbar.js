2b1ee95bce5074335a57678c9b68092657f36e43
Scott Maclure <scott@maclure.com.au>
2015-04-07 17:13:46 +1000

    [#77] Update to ensure data propagates to child components:
    * Stopped using computed property for 'upNumClass' due to issue with markup-driven component.
    * Observe changes to 'items' data when isDataModel is truthy, to update 'upNumClass'.
    * On parent component change, go through child components and explicitly call set().
    
    This is not ideal, further research needed in regards to nested child components.

--- src/components/ux-iconbar/ux-iconbar.js
+++ src/components/ux-iconbar/ux-iconbar.js
@@ -2,51 +2,55 @@ Ractive.extend({
 
 	template: RactiveF.templates['ux-iconbar'],
 
-	computed: {
+	/**
+	 * TODO Move to generic helpers location?
+	 * @returns {string} The number of child items as a css class, e.g. "one-up", "three-up", etc.
+	 */
+	calculateUpNum: function (num) {
 
-		/**
-		 * TODO Move to generic helpers location?
-		 * @returns {string} The number of child items as a css class, e.g. "one-up", "three-up", etc.
-		 */
-		upNum: function () {
+		var supportedWords = [
+			'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'
+		];
 
-			var num = 0;
-			var data = this.get();
-
-			// FIXME Bit of a hack for data-driven components.
-			// Understand why this occurs. Why has oninit not set "items" yet?
-			if (data.isDataModel) {
-				num = _.isArray(data.items) ? data.items.length : 0;
-			} else {
-				num = _.isArray(data.itemComponents) ? data.itemComponents.length : 0;
-			}
-
-			var supportedWords = [
-				'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'
-			];
-
-			if (!supportedWords[num]) {
-				console.error('ux-iconbar#numberToWord: num NOT supported: ' + num);
-				return '';
-			}
-
-			return supportedWords[num] + '-up';
-
+		if (!supportedWords[num]) {
+			console.error('ux-iconbar#numberToWord: num NOT supported: ' + num);
+			return '';
 		}
+
+		var returnValue = supportedWords[num] + '-up';
+
+		return returnValue;
 
 	},
 
 	oninit: function () {
 
-		var itemComponents = this.findAllComponents('ux-iconbaritem');
+		// We have to wait until oninit for markup-driven components to be setup correctly.
+		var itemComponents = this.findAllChildComponents('ux-iconbaritem');
+		this.set('upNumClass', this.calculateUpNum(itemComponents.length));
 
-		var childCount = itemComponents.length;
-		if (childCount < 1 || childCount > 8) {
-			console.error('ux-iconbar only supports between 1-8 items.');
+		if (this.get('isDataModel')) {
+			// Only observe on data-driven components.
+			this.observe('items', function (newValue) {
+				this.set('upNumClass', this.calculateUpNum(newValue.length));
+			});
 		}
 
-		// Store for later use.
-		this.set('itemComponents', itemComponents);
+		// FIXME Need to propagate change event down into child components, if data is changed.
+		// We shouldn't have to do this?
+		this.on('change', function (keypaths) {
+
+			if (keypaths.items) {
+
+				var itemComponents = this.findAllChildComponents('ux-iconbaritem');
+
+				_.each(itemComponents, function (component, i) {
+					component.set(keypaths.items[i]);
+				});
+
+			}
+
+		});
 
 	}
 
