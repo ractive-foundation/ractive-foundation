b85d381c3f182ca5225ef99a94f528e344864bad
Scott Maclure <scott@maclure.com.au>
2015-04-08 18:06:43 +1000

    Updated ux-demo-xhr to simulate an XHR response from server.

--- src/components/ux-demo-xhr/ux-demo-xhr.js
+++ src/components/ux-demo-xhr/ux-demo-xhr.js
@@ -2,8 +2,12 @@ Ractive.extend({
 
 	template: RactiveF.templates['ux-demo-xhr'],
 
+	/**
+	 * For demo purposes, just hard-wiring data here for initial load.
+	 */
 	data: function () {
 		return {
+			endpoint: '/use-cases/ux-demo-xhr/demo-post-endpoint.json',
 			title: 'Demo XHR Component',
 			intro: 'Demonstrate a top-level component that posts to an endpoint and gets data back.',
 			person: {
@@ -19,10 +23,20 @@ Ractive.extend({
 
 		this.findComponent('ux-button').on('doSomeXhr', function () {
 
-			self.set('person', {
-				name: 'new name',
-				id: '6666666'
-			});
+			// Grab latest data from component and send to the server.
+			var endpoint = self.get('endpoint');
+			var data = self.get('person');
+
+			// See https://github.com/pyrsmk/qwest for API details.
+			RactiveF.plugins.qwest
+				// Using get because ractive-foundation server only supports GET by default.
+				.get(endpoint, data)
+				.then(function (response) {
+					self.set('person', response.data.person);
+				})
+				.catch(function (err) {
+					debugger;
+				});
 
 		});
 
