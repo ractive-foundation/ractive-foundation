186a2c690f3be2602b98ad2f1457663cf257f862
Scott Maclure <scott@maclure.com.au>
2015-04-26 18:26:52 +1000

    WIP settig up scrollToNearestPage.

--- src/components/ux-page-swipe/ux-page-swipe.js
+++ src/components/ux-page-swipe/ux-page-swipe.js
@@ -7,7 +7,22 @@ Ractive.extend({
 	},
 
 	oninit: function () {
-		// TODO Compute $('.ux-page-swipe').scrollLeft for each "page" in the container.
+
+		this.totalPages = 0;
+		this.pageWidth = 0;
+
+		this.on('scrollToNearestPage', function (e) {
+			var currentPos = e.node.scrollLeft;
+			var currentPageIndex = Math.floor(currentPos /  this.pageWidth);
+			console.log('currentPage:', currentPageIndex + 1);
+			return false;
+		});
+
+	},
+
+	oncomplete: function () {
+		this.pageWidth = this.find('.page').scrollWidth;
+		this.totalPages = this.findAll('.page').length;
 	}
 
 });
