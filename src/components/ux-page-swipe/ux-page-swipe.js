c79c8bc59b7a92598615a3fdc5229bd511bce076
Scott Maclure <scott@maclure.com.au>
2015-04-26 18:01:57 +1000

    First pass at using viewport widths/heights and -webkit-overflow-scrolling: touch.

--- src/components/ux-page-swipe/ux-page-swipe.js
+++ src/components/ux-page-swipe/ux-page-swipe.js
@@ -6,30 +6,8 @@ Ractive.extend({
 		currentPage: 1
 	},
 
-	computed: {
-		isOnPage: function () {
-			return 'is-onPage' + this.get('currentPage');
-		}
-	},
-
 	oninit: function () {
-
-		this.on('nextPage', function (e) {
-			var nextPage = this.get('currentPage') + 1;
-			// FIXME Quick hack for bounds.
-			nextPage = nextPage > 5 ? 5 : nextPage;
-			this.set('currentPage', nextPage);
-			return false;
-		});
-
-		this.on('prevPage', function (e) {
-			var prevPage = this.get('currentPage') -1 ;
-			// FIXME Quick hack for bounds.
-			prevPage = prevPage < 1 ? 1 : prevPage;
-			this.set('currentPage', prevPage);
-			return false;
-		});
-
+		// TODO Compute $('.ux-page-swipe').scrollLeft for each "page" in the container.
 	}
 
 });
