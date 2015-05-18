4340a9c601204913b5bd5ff347e62df9927105f1
Antony Isoardi <antony@singtel.com>
2015-05-18 16:04:19 +0800

    Applied JS for ux-top-bar sticky behaviour as well as fixed. Created new use case for ux-top-bar sticky

--- src/components/ux-top-bar/ux-top-bar.js
+++ src/components/ux-top-bar/ux-top-bar.js
@@ -1,8 +1,13 @@
+
+
 Ractive.extend({
 	template: RactiveF.templates['ux-top-bar'],
 	oninit: function () {
 
 		var self = this;
+		var topbar;
+
+		self.set('yPos', 0);
 
 		this.on('toggleMenu', function(e) {
 
@@ -16,6 +21,50 @@ Ractive.extend({
 
 		});
 
+
+		window.onscroll = function(e) {
+
+			topbar = self.find('.top-bar');
+
+			function getYPos(elem) {
+				var box = elem.getBoundingClientRect();
+				var body = document.body;
+				var docEl = document.documentElement;
+				var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+				var clientTop = docEl.clientTop || body.clientTop || 0;
+				var top  = box.top +  scrollTop - clientTop;
+				return Math.round(top);
+			}
+
+
+			if (self.get('isSticky')) {
+				if (window.pageYOffset > self.get('yPos')) {
+					self.set('isFixed', true);
+				} else {
+					self.set('isFixed', false);
+					self.set('yPos', getYPos(topbar));
+				}
+			}
+
+		};
+
+	},
+	oncomplete: function () {
+
+		var self = this;
+
+		function getYPos(elem) {
+			var box = elem.getBoundingClientRect();
+			var body = document.body;
+			var docEl = document.documentElement;
+			var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+			var clientTop = docEl.clientTop || body.clientTop || 0;
+			var top  = box.top +  scrollTop - clientTop;
+			return Math.round(top);
+		}
+
+		var topbar = self.find('.top-bar');
+		self.set('yPos', getYPos(topbar));
 	}
 
 });
