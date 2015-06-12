ce18a6382de4a36a6ed7fd7dfceac271a9f3a152
Nathan Flew <nath@nathf.com>
2015-06-12 13:19:27 +1000

    Refactor base further to remove prototype extension

--- src/ractivef.base.js
+++ src/ractivef.base.js
@@ -1,64 +1,61 @@
 RactiveF = {
 	components: {},
 	templates: {},
+	getInstance: function () {
+		return Ractive.extend(RactiveF);
+	},
 	forge: function (options) {
 		options = options || {};
-
-		if (!Ractive.prototype.findAllChildComponents) {
-			_.mixin(Ractive.prototype, RactiveF.mixins);
-		}
-
-		var Instance = Ractive.extend(this);
+		var Instance = RactiveF.getInstance();
 		return new Instance(options);
 	},
 
-	mixins: {
+	/*
+	 * When working with nested components we only want to find child
+	 * components, not all decendants.
+	 * @param name
+	 */
+	findAllChildComponents: function (name) {
+		return _.filter(this.findAllComponents(name), function (component) {
+			return this._guid === component.parent._guid;
+		}.bind(this));
+	},
 
-			/*
-			 * When working with nested components we only want to find child
-			 * components, not all decendants.
-			 * @param name
-			 */
-			findAllChildComponents: function (name) {
-				return _.filter(this.findAllComponents(name), function (component) {
-					return this._guid === component.parent._guid;
-				}.bind(this));
-			},
+	/**
+	 * If we have a "datamodel" property, that should override any other data.
+	 * This is now a "data-driven" component.
+	 * isDataModel is a flag for hbs logic, on whether to use datamodel data or call {{yield}}.
+	 * @see http://docs.ractivejs.org/latest/ractive-reset
+	 *
+	 * TODO Understand the difference between rendering components off the page vs nested inside others.
+	 * onconstruct has empty opts for the latter.
+	 */
+	onconstruct: function (opts) {
+		if (opts.data && opts.data.datamodel) {
+			var datamodel = _.cloneDeep(opts.data.datamodel);
+			datamodel.isDataModel = true;
+			opts.data = _.assign(opts.data, datamodel);
+			delete opts.data.datamodel;
+		}
+	},
 
-			/**
-			 * If we have a "datamodel" property, that should override any other data.
-			 * This is now a "data-driven" component.
-			 * isDataModel is a flag for hbs logic, on whether to use datamodel data or call {{yield}}.
-			 * @see http://docs.ractivejs.org/latest/ractive-reset
-			 *
-			 * TODO Understand the difference between rendering components off the page vs nested inside others.
-			 * onconstruct has empty opts for the latter.
-			 */
-			onconstruct: function (opts) {
-				if (opts.data && opts.data.datamodel) {
-					var datamodel = _.cloneDeep(opts.data.datamodel);
-					datamodel.isDataModel = true;
-					opts.data = _.assign(opts.data, datamodel);
-					delete opts.data.datamodel;
-				}
-			},
+	/**
+	 * For any data-driven component - if something sets 'datamodel', lift that into root scope.
+	 */
+	onrender: function () {
 
-			/**
-			 * For any data-driven component - if something sets 'datamodel', lift that into root scope.
-			 */
-			onrender: function () {
-
-				// Wait for parent component to set "datamodel" and then map that back into data again.
-				this.observe('datamodel', function (newDataModel) {
-					if (newDataModel) {
-						// Lift datamodel data into root data scope.
-						this.set(newDataModel);
-					}
-				});
-
+		// Wait for parent component to set "datamodel" and then map that back into data again.
+		this.observe('datamodel', function (newDataModel) {
+			if (newDataModel) {
+				// Lift datamodel data into root data scope.
+				this.set(newDataModel);
 			}
+		});
 
 	}
 
-
 };
+
+/* jshint ignore:start */
+var Component = RactiveF.getInstance();
+/* jshint ignore:end */
