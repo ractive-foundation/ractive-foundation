e48119a9876921bf690101d37ca8f5358364a280
Nathan Flew <nath@nathf.com>
2015-06-18 19:32:12 +1000

    #140 Add cross platform version check

---
+++ bin/versionCheck.js
@@ -0,0 +1,33 @@
+const MINIMUM_VERSION = '0.12.0';
+
+var compareVersions = function (installed, required) {
+    var a = installed.split('.');
+    var b = required.split('.');
+
+    for (var i = 0; i < a.length; ++i) {
+        a[i] = Number(a[i]);
+    }
+    for (var i = 0; i < b.length; ++i) {
+        b[i] = Number(b[i]);
+    }
+    if (a.length == 2) {
+        a[2] = 0;
+    }
+
+    if (a[0] > b[0]) return true;
+    if (a[0] < b[0]) return false;
+
+    if (a[1] > b[1]) return true;
+    if (a[1] < b[1]) return false;
+
+    if (a[2] > b[2]) return true;
+    if (a[2] < b[2]) return false;
+
+    return true;
+}
+
+if (!compareVersions(process.version, MINIMUM_VERSION)) {
+  console.log('Invalid node version. Version %s or greater is required.'.replace('%s', MINIMUM_VERSION));
+  console.log('Current version: %s'.replace('%s', process.version));
+  process.exit(1);
+}
