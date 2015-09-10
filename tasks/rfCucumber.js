72d0eef74a0637be55747cb21db8b6d08126281e
Nathan Flew <nath@nathf.com>
2015-09-10 22:18:15 +1000

    #161 Separate selenium and cucumber

--- tasks/rfCucumber.js
+++ tasks/rfCucumber.js
@@ -1,11 +1,7 @@
-var selenium = require('selenium-standalone');
 var gutil = require('gulp-util');
-var http = require('http');
 var through = require('through2');
 var Cucumber = require('cucumber');
 var glob = require('simple-glob');
-var async = require('async');
-var Q = require('q');
 
 /**
  * Combination of gulp-cucumber and gulp-webdriverio
@@ -18,13 +14,8 @@ var Q = require('q');
  */
 module.exports = function (options) {
 
-	var seleniumServer;
-	var isSeleniumServerRunning;
-	var seleniumOptions = options.seleniumOptions || {};
-	var seleniumInstallOptions = options.seleniumInstallOptions || {};
 	var argv = ['node', 'cucumber-js'];
 	var format = options.format || 'pretty';
-	var server;
 	var files = [];
 	var features = [];
 	var runOptions = [];
@@ -43,106 +34,18 @@ module.exports = function (options) {
 		runOptions.push(file);
 	});
 
-	/**
-	 * Helper function to see if Selenium server is already running.
-	 * @param callback
-	 */
-	var pingSelenium = function(callback) {
-
-		gutil.log(gutil.colors.gray('Checking if Selenium server is running'));
-
-		var opts = {
-			host: options.host || 'localhost',
-			port: options.port || 4444,
-			path: '/wd/hub/status'
-		};
-
-		http.get(opts, function() {
-			gutil.log(gutil.colors.green('Selenium server is running'));
-			isSeleniumServerRunning = true;
-			callback(null);
-		}).on('error', function() {
-			gutil.log(gutil.colors.gray('Selenium server is not running'));
-			callback(null);
-		});
-
-	};
-
-	/**
-	 * Start the Selenium server
-	 * @param callback
-	 */
-	var startServer = function(callback) {
-
-		if (!server && !isSeleniumServerRunning && !options.nospawn) {
-
-			gutil.log('Starting', gutil.colors.cyan('\'selenium standalone server\''));
-
-			server = selenium.start(seleniumOptions, function(err, child) {
-				if (err) {
-					return callback(err);
-				}
-
-				gutil.log(gutil.colors.green('Selenium server successfully started'));
-				seleniumServer = child;
-				isSeleniumServerRunning = true;
-				callback(null);
-			});
-
-		} else {
-			gutil.log(gutil.colors.gray('Standalone server is running'));
-			callback(null);
-		}
-
-	};
-
-	/**
-	 * Process to kill server on completion
-	 */
-	var killServer = function () {
-		var promise;
-		if (seleniumServer) {
-			promise = Q.promise(function (resolve, reject) {
-				seleniumServer.kill();
-				seleniumServer.on('close', function (code, signal) {
-					gutil.log('Finished', gutil.colors.cyan('\'selenium standalone server\''));
-					return resolve(signal);
-				});
-				seleniumServer.on('error', function (code, signal) {
-					gutil.log(gutil.colors.red('Failed to kill server!'));
-					return reject(signal);
-				});
-			});
-		} else {
-			promise = Q.resolve();
-			gutil.log(gutil.colors.red('Cannot kill standalone server.'));
-		}
-
-		return promise;
-	};
-
-	/**
-	 * Install webdrivers for selenium to use.
-	 * @param callback
-	 */
-	var installDrivers = function (callback) {
-		gutil.log(gutil.colors.gray('Installing driver(s) if needed'));
-		selenium.install(seleniumInstallOptions, function(err) {
-			if (err) {
-				return callback(err);
-			}
-
-			gutil.log(gutil.colors.green('Driver installed!'));
-			callback(null);
-		});
-	};
-
 	// Run all cucumber tests at once.
 	// TODO Run them one by one. Fail early.
 	var runTests = function (callback) {
+
+		var stream = this;
+		argv.push.apply(argv, runOptions);
+		argv.push.apply(argv, features);
+
 		Cucumber.Cli(argv).run(function(succeeded) {
 			if (succeeded) {
-				callback(null);
+				stream.emit('end');
+				return callback();
 			} else {
 				callback(new Error('Cucumber tests failed!'));
 			}
@@ -162,38 +65,6 @@ module.exports = function (options) {
 		callback();
 	};
 
-	var run = function(callback) {
-
-		var stream = this;
-
-		argv.push.apply(argv, runOptions);
-		argv.push.apply(argv, features);
-
-		// Order of events.
-		async.waterfall([
-			pingSelenium.bind(stream),
-			installDrivers.bind(stream),
-			startServer.bind(stream),
-			runTests.bind(stream)
-		], function (err) {
-
-			// Kill server regardless of result.
-			killServer().then(function () {
-				if (!err) {
-					return callback();
-				}
-
-				var error = new gutil.PluginError('rf-cucumber', {
-					message: err,
-					showStack: false
-				});
-
-				callback(error);
-				stream.emit('end');
-			}).catch(console.log.bind(console));
-		});
-	};
-
-	return through.obj(collect, run);
+	return through.obj(collect, runTests);
 
 };
---
+++ tasks/seleniumServer.js
@@ -0,0 +1,126 @@
+var selenium = require('selenium-standalone');
+var gutil = require('gulp-util');
+var http = require('http');
+var Q = require('q');
+
+module.exports = function (options) {
+
+	var server;
+	var seleniumServer;
+	var isSeleniumServerRunning;
+	var options = options || {};
+	var seleniumOptions = options.seleniumOptions || {};
+	var seleniumInstallOptions = options.seleniumInstallOptions || {};
+
+	/**
+	 * Helper function to see if Selenium server is already running.
+	 * @param callback
+	 */
+	var pingSelenium = function(options) {
+		options = options || {};
+		return Q.Promise(function (resolve, reject) {
+			gutil.log(gutil.colors.gray('Checking if Selenium server is running'));
+
+			var opts = {
+				host: options.host || 'localhost',
+				port: options.port || 4444,
+				path: '/wd/hub/status'
+			};
+
+			http.get(opts, function() {
+				gutil.log(gutil.colors.green('Selenium server is running'));
+				isSeleniumServerRunning = true;
+				resolve(isSeleniumServerRunning);
+			}).on('error', function() {
+				gutil.log(gutil.colors.gray('Selenium server is not running'));
+				resolve('Server not running');
+			});
+		});
+	};
+
+	/**
+	 * Start the Selenium server
+	 * @param callback
+	 */
+	var startServer = function(options) {
+		options = options || seleniumOptions;
+		return Q.Promise(function (resolve, reject) {
+
+			if (server && isSeleniumServerRunning || options.nospawn) {
+				gutil.log(gutil.colors.gray('Standalone server is running'));
+				return reject();
+			}
+
+			gutil.log('Starting', '\'' + gutil.colors.cyan('selenium standalone server') + '\'...');
+
+			server = selenium.start(options, function(err, child) {
+				if (err) {
+					return reject(err);
+				}
+
+				gutil.log(gutil.colors.green('Selenium server successfully started'));
+				seleniumServer = child;
+				isSeleniumServerRunning = true;
+				return resolve(child);
+			});
+		});
+	};
+
+	/**
+	 * Process to kill server on completion
+	 */
+	var killServer = function () {
+		return Q.Promise(function (resolve, reject) {
+			if (!seleniumServer) {
+				gutil.log(gutil.colors.red('Cannot kill standalone server.'));
+				return reject();
+			}
+
+			seleniumServer.kill();
+			seleniumServer.on('close', function (code, signal) {
+				gutil.log('Finished', '\'' + gutil.colors.cyan('selenium standalone server') + '\'...');
+				return resolve(signal);
+			});
+			seleniumServer.on('error', function (code, signal) {
+				gutil.log(gutil.colors.red('Failed to kill server!'));
+				return reject(signal);
+			});
+		});
+	};
+
+	/**
+	 * Install webdrivers for selenium to use.
+	 * @param callback
+	 */
+	var installDrivers = function () {
+		return Q.Promise(function (resolve, reject) {
+			gutil.log(gutil.colors.gray('Installing driver(s) if needed'));
+			selenium.install(seleniumInstallOptions, function(err) {
+				if (err) {
+					return reject(err);
+				}
+
+				gutil.log(gutil.colors.green('Driver installed!'));
+				return resolve();
+			});
+		});
+	};
+
+
+	var init = function () {
+		return pingSelenium(options).then(function () {
+			return options.install ? installDrivers(seleniumInstallOptions) : Q.resolve();
+		}).then(function () {
+			return startServer(seleniumOptions);
+		});
+	};
+
+	return {
+		installDrivers: installDrivers,
+		killServer: killServer,
+		startServer: startServer,
+		pingSelenium: pingSelenium,
+		init: init
+	};
+
+};
