55d149ea476bdb05befa3e553f34e01bfa93c6a0
Zac Xu <zac.xu@optus.com.au>
2015-09-16 13:59:32 +1000

    Added close handling

--- src/components/ux-alert/ux-alert.js
+++ src/components/ux-alert/ux-alert.js
@@ -1,3 +1,9 @@
 Ractive.extend({
-	template: Ractive.defaults.templates['ux-alert']
+	template: Ractive.defaults.templates['ux-alert'],
+	oninit: function ( options ) {
+		this.on( 'closeClicked', function () {
+			this.teardown();
+			return false;
+		});
+	}
 });
