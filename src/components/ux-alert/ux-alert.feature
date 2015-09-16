88f7d49a8f4165428dc0ada01e62f490b8c53eef
Zac Xu <zac.xu@optus.com.au>
2015-09-16 12:24:17 +1000

    Added test

--- src/components/ux-alert/ux-alert.feature
+++ src/components/ux-alert/ux-alert.feature
@@ -1,4 +1,6 @@
 Feature: ux-alert test suite
 
   Scenario: Loading ux-alert
-    Given I have loaded component "ux-alert" with use case "UseCase"
+    Given I have loaded component "ux-alert" with use case "standard"
+    Then the element "div" should be displayed
+    Then the element "div" should have the "className" of "alert-box"
--- src/components/ux-alert/ux-alert.steps.js
+++ src/components/ux-alert/ux-alert.steps.js
@@ -3,16 +3,44 @@ module.exports = function () {
 	// Load standard world object to be 'this' in steps.
 	this.World = require('../../world').World;
 
-	this.Before(function (callback) {
-		this.component = {};
-		this.component.container = '#childComponent ';
+	var component = { container: '#childComponent' };
+		component.div = {
+			selector: component.container + ' div',
+			attribs: { className: 'class' }
+		};
 
-		callback();
-	});
 
 	this.Given(/^I have loaded component "([^"]*)" with use case "([^"]*)"$/,
 		function (componentName, useCase, callback) {
 			this.client.loadComponentWithUseCase(componentName, useCase, callback);
 	});
 
+	this.Then(/^the element "([^"]*)" should be displayed$/, function (semanticName, callback) {
+		var self = this;
+		this.client.isExisting(component[semanticName].selector, function (err, isExisting) {
+			try {
+				self.assert(isExisting);
+				callback();
+			} catch (e) {
+				callback.fail('Assertion failed, element not found');
+			}
+		});
+	});
+
+	this.Then(/^the element "([^"]*)" should have the "([^"]*)" of "([^"]*)"$/,
+		function (semanticName, attribName, attribValue, callback) {
+		var self = this;
+		this.client.getAttribute(
+			component[semanticName].selector,
+			component[semanticName].attribs[attribName],
+			function (err, attr) {
+				try {
+					self.assert.deepEqual(attr, attribValue);
+					callback();
+				} catch (e) {
+					callback.fail(e.name + ' ' + e.message);
+				}
+			}
+		);
+	});
 };
