bafd94d48a0227ed9bf25428e68092a7cd1a5ffb
Zac Xu <zac.xu@optus.com.au>
2015-09-18 10:28:31 +1000

    Refactored ux-alert test

--- src/components/ux-alert/ux-alert.steps.js
+++ src/components/ux-alert/ux-alert.steps.js
@@ -3,66 +3,13 @@ module.exports = function () {
 	// Load standard world object to be 'this' in steps.
 	this.World = require('../../world').World;
 
-	var component = { container: '#childComponent' };
-	component.alert = {
-		selector: component.container + ' div',
-		attribs: { className: 'class' }
-		};
+	require('../../support/steps').call(this);
 	
 	this.Before(function (callback) {
 		this.component = {};
-		this.component.cross = '.close';
-
+		this.component.container	= '#childComponent';
+		this.component.cross		= this.component.container + ' .close';
+		this.component.alertBox		= this.component.container + ' .alert-box';
 		callback();
-
-	});
-
-	this.Given(/^I have loaded component "([^"]*)" with use case "([^"]*)"$/,
-		function (componentName, useCase, callback) {
-			this.client.loadComponentWithUseCase(componentName, useCase, callback);
-	});
-
-	this.Then(/^the element "([^"]*)" should be displayed$/, function (semanticName, callback) {
-		var self = this;
-		this.client.isExisting(component[semanticName].selector, function (err, isExisting) {
-			try {
-				self.assert(isExisting);
-				callback();
-			} catch (e) {
-				callback.fail('Assertion failed, element not found');
-			}
-		});
-	});
-
-	this.Then(/^the element "([^"]*)" should have the "([^"]*)" of "([^"]*)"$/,
-		function (semanticName, attribName, attribValue, callback) {
-		var self = this;
-		this.client.getAttribute(
-			component[semanticName].selector,
-			component[semanticName].attribs[attribName],
-			function (err, attr) {
-				try {
-					self.assert.deepEqual(attr, attribValue);
-					callback();
-				} catch (e) {
-					callback.fail(e.name + ' ' + e.message);
-				}
-			}
-		);
-	});
-
-	this.When(/^I click "([^"]*)"$/, function (element, callback) {
-		this.client.waitForExist(this.component.cross, this.defaultTimeout).then(function () {
-			return this.client.click(this.component.cross);
-		}.bind(this)).then(function () {
-			callback();
-		}).catch(callback);
-	});
-
-	this.Then(/^"([^"]*)" will NOT be visible$/, function (element, callback) {
-		this.client.isVisible(component[element].selector).then(function (isVisible) {
-			var e = (isVisible) ? new Error('Element is visible! Selector: ' + this.component.cross) : void 0;
-			callback(e);
-		}.bind(this)).catch(callback);
 	});
 };
