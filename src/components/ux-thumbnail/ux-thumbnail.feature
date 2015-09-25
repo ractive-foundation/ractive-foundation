96cb1e39719f32fab90a906fb11a3f3be8ac150a
Ivan Wills <Ivan.Wills2@optus.com.au>
2015-09-25 16:07:08 +1000

    Getting a test running

--- src/components/ux-thumbnail/ux-thumbnail.feature
+++ src/components/ux-thumbnail/ux-thumbnail.feature
@@ -1,4 +1,6 @@
 Feature: ux-thumbnail test suite
 
-  Scenario: Loading ux-thumbnail
-    Given I have loaded component "ux-thumbnail" with use case "UseCase"
+	Scenario: Loading ux-thumbnail
+		Given I have loaded component "ux-thumbnail" with use case "dataDriven"
+		Then the element "img" should have attribute "src" containing "http://placekitten.com/g/1500/500"
+		And the element "a" should have attribute "href" containing "#"
--- src/components/ux-thumbnail/ux-thumbnail.steps.js
+++ src/components/ux-thumbnail/ux-thumbnail.steps.js
@@ -2,10 +2,13 @@ module.exports = function () {
 
 	// Load standard world object to be 'this' in steps.
 	this.World = require('../../world').World;
+	require('../../support/steps').call(this);
 
 	this.Before(function (callback) {
 		this.component = {};
 		this.component.container = '#childComponent ';
+		this.component.img = this.component.container + 'img';
+		this.component.a   = this.component.container + 'a';
 
 		callback();
 	});
