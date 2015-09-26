9c07d64ccc89b2348971e50ecc2e254dce8ee28a
Ivan Wills <ivan.wills@gmail.com>
2015-09-27 08:54:45 +1000

    Changed all tests to use default steps file

--- src/components/ux-sidenav/ux-sidenav.steps.js
+++ src/components/ux-sidenav/ux-sidenav.steps.js
@@ -2,6 +2,7 @@ module.exports = function () {
 
 	// Load standard world object to be 'this' in steps.
 	this.World = require('../../world').World;
+	require('../../support/steps').call(this);
 
 	require('../../support/steps').call(this);
 
