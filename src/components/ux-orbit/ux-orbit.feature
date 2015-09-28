756d60db5c491d707568cb3e4c9d2755a642c1c3
Ivan Wills <ivan.wills@gmail.com>
2015-09-28 19:22:28 +1000

    #175 Generalised slide count step

--- src/components/ux-orbit/ux-orbit.feature
+++ src/components/ux-orbit/ux-orbit.feature
@@ -1,5 +1,5 @@
 Feature: ux-orbit test suite
 
-  Scenario: Loading ux-orbit
-    Given I have loaded component "ux-orbit" with use case "dataDriven"
-    Then I should see 3 slides
+	Scenario: Loading ux-orbit
+		Given I have loaded component "ux-orbit" with use case "dataDriven"
+		Then there should be 3 of the element "slides"
--- src/components/ux-orbit/ux-orbit.steps.js
+++ src/components/ux-orbit/ux-orbit.steps.js
@@ -2,11 +2,13 @@ module.exports = function () {
 
 	// Load standard world object to be 'this' in steps.
 	this.World = require('../../world').World;
+	require('../../support/steps').call(this);
 
 	this.Before(function (callback) {
 		this.component = {};
 		this.component.container = '#childComponent ';
-		this.component.bullets = this.component.container + '.orbit-bullets';
+		this.component.bullets = this.component.container + '.orbit-bullets ';
+		this.component.slides  = this.component.bullets + 'li';
 
 		callback();
 	});
@@ -14,23 +16,6 @@ module.exports = function () {
 	this.Given(/^I have loaded component "([^"]*)" with use case "([^"]*)"$/,
 		function (componentName, useCase, callback) {
 			this.client.loadComponentWithUseCase(componentName, useCase, callback);
-	});
-
-	this.Then(/^I should see (\d+) slides$/, function (numElements, callback) {
-		var selector = this.component.bullets + ' li';
-
-		this.client.waitForExist(selector, 500).then(function () {
-			return this.client.elements(selector);
-		}.bind(this)).then(function (elements) {
-			try {
-				this.assert.equal(elements.value.length, numElements);
-				callback();
-			} catch(e) {
-				callback(e);
-			}
-		}.bind(this)).catch(function (e){
-			callback(e);
-		});
 	});
 
 };
