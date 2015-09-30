7f054a3772ba2f2c1ac6a454f11be95e226714a6
Indra Arifin <indra.arifin@optus.com.au>
2015-09-30 16:47:04 +1000

    #211: WIP (30 Sep)

--- src/components/ux-tooltip/README.md
+++ src/components/ux-tooltip/README.md
@@ -1,1 +1,15 @@
-> ux-tooltip Documentation
+This is a straight-forward Ractive-friendly tooltip with foundation styling.
+
+To enable a full Ractive experience, the tooltip container is pre-rendered during page load rather than being created
+on event trigger. This way, we no longer need to pass on any tooltip content data into html to be read by Javascript.
+
+This approach also simplifies the ability to show HTML components inside the tooltip content.
+
+Minor styling modification is added for this change.
+
+To Do:
+- Recalculate the position of the tooltip element and the tip arrow on hover.
+- Show/remove on different screen size.
+- ...
+
+Read [Foundation's tooltip.html](http://foundation.zurb.com/docs/components/tooltips.html) docs for original details.
--- src/components/ux-tooltip/ux-tooltip.js
+++ src/components/ux-tooltip/ux-tooltip.js
@@ -1,13 +1,74 @@
 Ractive.extend({
 	template: Ractive.defaults.templates['ux-tooltip'],
 
-    isolated: true,
+	isolated: true,
 
-    oninit: function () {
+	decorators: {
+		tooltip: /*tooltipDecorator */ function(node, content) {
+			// this is supposed to be in tooltipDecorator, eg tooltip.element, tooltip.className, etc.
+			var config_element = 'span',
+				config_className = 'tooltip',
+				config_offsetX = 0,
+				config_offsetY = -20;
 
-        this.on('tooltipHovered', function (item) {
-            item.toggle('open');
-        }.bind(this));
+			var tooltip, handlers, eventName;
+			handlers = {
+				mouseover: function() {
+					tooltip = document.createElement( config_element );
+					tooltip.className = config_className;
+					tooltip.textContent = content;
 
-    }
+					node.parentNode.insertBefore( tooltip, node );
+				},
+
+				mousemove: function ( event ) {
+					tooltip.style.left = event.clientX + config_offsetX + 'px';
+					tooltip.style.top = ( event.clientY - tooltip.clientHeight + config_offsetY ) + 'px';
+				},
+
+				mouseleave: function () {
+					tooltip.parentNode.removeChild( tooltip );
+				}
+			};
+
+			for ( eventName in handlers ) {
+				if (handlers.hasOwnProperty(eventName)) {
+					node.addEventListener(eventName, handlers[eventName], false);
+				}
+			}
+
+			return {
+				teardown: function () {
+					for ( eventName in handlers ) {
+						if ( handlers.hasOwnProperty( eventName ) ) {
+							node.removeEventListener( eventName, handlers[ eventName ], false );
+						}
+					}
+				}
+			};
+		}
+	},
+
+	computed: {
+		guid: function () {
+			return this._guid;
+		}
+	},
+
+	oninit: function (options) {
+
+		this.on('tooltipHovered', function (srcItem) {
+			this.toggle('open');
+			/*
+			console.log(this);
+			console.log(srcItem);
+
+			var attrs = srcItem.node.attributes,
+				selector = attrs['data-selector'].value,
+				content = attrs['data-content'].value;
+			var tooltip = this.decorators.tooltip.settings.tip_template(selector, content);
+			*/
+			return false;
+		});
+	}
 });
