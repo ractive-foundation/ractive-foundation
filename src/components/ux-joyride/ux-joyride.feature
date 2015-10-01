63124aeebe4103159d2a939c124baee759656f85
Vishaka Wele Uhulanage <vishaka.piyumali@optus.com.au>
2015-10-01 16:17:18 +1000

    [#212] init new component

---
+++ src/components/ux-joyride/ux-joyride.feature
@@ -0,0 +1,4 @@
+Feature: ux-joyride test suite
+
+  Scenario: Loading ux-joyride
+    Given I have loaded component "ux-joyride" with use case "UseCase"
---
+++ src/components/ux-joyride/ux-joyride.steps.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+
+	// Load standard world object to be 'this' in steps.
+	this.World = require('../../world').World;
+
+	// Load shared library of step definitions. Use these first!
+	require('../../support/steps').call(this);
+
+	this.Before(function (callback) {
+
+		this.component = {};
+		this.component.container = '#childComponent ';
+
+		callback();
+
+	});
+
+};
