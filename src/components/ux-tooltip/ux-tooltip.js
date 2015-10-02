7c908d9f2136d22405fcd0f72e367fa2f2a627e2
Indra Arifin <indra.arifin@optus.com.au>
2015-10-02 14:37:02 +1000

    #211: A bit more cleaning.

--- src/components/ux-tooltip/ux-tooltip.js
+++ src/components/ux-tooltip/ux-tooltip.js
@@ -1,22 +1,3 @@
 Ractive.extend({
 	template: Ractive.defaults.templates['ux-tooltip'],
-
-	isolated: true,
-
-	computed: {
-		guid: function () {
-			return this._guid;
-		}
-	},
-
-	oninit: function () {
-		this.on('tooltipHovered', function (srcItem) {
-			this.toggle('open');
-			return false;
-		});
-	},
-
-	oncomplete: function () {
-		console.log(this);
-	}
 });
