159384f810ba65c4b0c6d40df7f700accd886c78
Tanuja Yadupati <tanuja.yadupati@optus.com.au>
2015-10-08 15:30:57 +1100

    [#203]-added button group

---
+++ src/components/ux-button-group/ux-button-group.feature
@@ -0,0 +1,4 @@
+Feature: ux-button-group test suite
+
+  Scenario: Loading ux-button-group
+    Given I have loaded component "ux-button-group" with use case "UseCase"
---
+++ src/components/ux-button-groupitem/ux-button-groupitem.js
@@ -0,0 +1,16 @@
+Ractive.extend({
+	template: Ractive.defaults.templates['ux-button-groupitem'],
+
+	isolate: true,
+
+	anchorClicked: function() {
+		// if a click event is specified propagate the click event
+		if (this.get('onclick')) {
+			console.log('Firing event');
+			this.fire(this.get('onclick'), this);
+		}
+
+		// prevent bubbling
+		return true;
+	}
+});
