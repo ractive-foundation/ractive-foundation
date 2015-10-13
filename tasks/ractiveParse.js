0ac424fd2d938d2f4476996adac4265ffa34d6fa
Ivan Wills <ivan.wills@gmail.com>
2015-10-13 22:01:46 +1100

    Refactored out template processing

--- tasks/ractiveParse.js
+++ tasks/ractiveParse.js
@@ -7,6 +7,29 @@ var PluginError = gulputil.PluginError;
 
 const PLUGIN_NAME = 'gulp-ractive-parse';
 
+function ractiveTemplateParse(file, options) {
+	var componentName;
+	if (options.test) {
+		var parts = file.history[0].split(path.sep).slice(-3);
+		componentName = parts[0] + '-' + parts[2].replace(/[.]hbs$/, '');
+	}
+	else {
+		componentName = file.history[0].split(path.sep).slice(-2)[0];
+	}
+
+	var filecontents = '';
+
+	filecontents = String(file.contents);
+
+	//Parse template in Ractive
+	filecontents = Ractive.parse(filecontents, options);
+	filecontents = JSON.stringify(filecontents);
+
+	filecontents = options.prefix + '[\'' + componentName + '\'] = ' + filecontents + ';';
+
+	return filecontents;
+}
+
 function gulpRactive(options) {
 	var stream = through.obj(function (file, enc, callback) {
 		if (file.isStream()) {
@@ -14,27 +37,8 @@ function gulpRactive(options) {
 			return callback();
 		}
 
-		var componentName;
-		if (options.test) {
-			var parts = file.history[0].split(path.sep).slice(-3);
-			componentName = parts[0] + '-' + parts[2].replace(/[.]hbs$/, '');
-		}
-		else {
-			componentName = file.history[0].split(path.sep).slice(-2)[0];
-		}
-
-		var filecontents = '';
-
 		try {
-			filecontents = String(file.contents);
-
-			//Parse template in Ractive
-			filecontents = Ractive.parse(filecontents, options);
-			filecontents = JSON.stringify(filecontents);
-
-			filecontents = options.prefix + '[\'' + componentName + '\'] = ' + filecontents + ';';
-
-			file.contents = new Buffer(filecontents);
+			file.contents = new Buffer(ractiveTemplateParse(file, options));
 			this.push(file);
 		}
 		catch (e) {
