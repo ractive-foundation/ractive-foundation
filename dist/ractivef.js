432fe6dcee2dc61ad03fe09778df1dfaeef347ac
Scott Maclure <scott.maclure1@optus.com.au>
2015-10-15 16:46:44 +1100

    Dist.

--- dist/ractivef.js
+++ dist/ractivef.js
@@ -1,6 +1,6 @@
 /**
  * ractive-foundation - Ractive components for Foundation 5
- * @version 0.^.0
+ * @version 0.6.0
  * @link https://github.com/ractive-foundation/ractive-foundation
  * @license MIT
  */
