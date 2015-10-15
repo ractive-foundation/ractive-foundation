752890d8f29ae3cc7b64ba1299c29b822488f5fc
Indra Arifin <indra.arifin@optus.com.au>
2015-10-16 09:51:29 +1100

    #234: Extend tests

--- src/components/ux-iconbar/ux-iconbar.steps.js
+++ src/components/ux-iconbar/ux-iconbar.steps.js
@@ -8,6 +8,12 @@ module.exports = function () {
 		this.component = {};
 		this.component.container = '#childComponent';
 		this.component.iconbar = this.component.container + ' .icon-bar';
+		this.component.item = this.component.iconbar + ' .item';
+		this.component.firstItem = this.component.item + ':nth-child(1)';
+		this.component.firstItemLabel = this.component.firstItem + ' label';
+		this.component.thirdItem = this.component.item + ':nth-child(3)';
+		this.component.thirdItemLabel = this.component.thirdItem + ' label';
+		this.component.fourthItem = this.component.item + ':nth-child(4)';
 		callback();
 	});
 
