ab8b5c831d9145b3f51fca0f19e6fe852508e927
scott-dickson <scott.dickson72@gmail.com>
2015-10-26 21:42:44 +1100

    - fixed jscs error - missing space after function

--- src/components/ux-alert/ux-alert.js
+++ src/components/ux-alert/ux-alert.js
@@ -1,7 +1,7 @@
 Ractive.extend({
 	template: Ractive.defaults.templates['ux-alert'],
 	oninit: function (options) {
-		this.on('closeClicked', function() {
+		this.on('closeClicked', function () {
 			this.teardown();
 			return false;
 		});
