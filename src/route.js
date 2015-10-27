b698f0d599517d1da3b50854b15efdcd776a37ee
scott-dickson <scott.dickson72@gmail.com>
2015-10-28 00:09:30 +1100

    - improve indentation

--- src/route.js
+++ src/route.js
@@ -7,13 +7,13 @@ page('/component/:name/use-case/:useCase', function (ctx) {
 	superagent.get(url.join(''), function (err, res) {
 		var data = res.body && res.body.data || {},
 			config = {
-			el: '#childComponent',
-			data: function () {
-				return _.extend(data, {
-					isDataModel: true
-				});
-			}
-		};
+				el: '#childComponent',
+				data: function () {
+					return _.extend(data, {
+						isDataModel: true
+					});
+				}
+			};
 
 		if (res.body && res.body.template) {
 			config.template = Ractive.defaults.templates[res.body.template];
