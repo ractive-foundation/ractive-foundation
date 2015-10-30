02631b481007e0a41961cba74e36b9df8f93afd5
Fakhruddin Zakiuddin <fakhruddin.zakiuddin@optus.com.au>
2015-10-30 15:53:15 +1100

    [210] updated the dataModel, markup and styles

--- src/components/ux-reveal/ux-reveal.js
+++ src/components/ux-reveal/ux-reveal.js
@@ -1,6 +1,33 @@
 Ractive.extend({
 	template: Ractive.defaults.templates['ux-reveal'],
 	data: {
-		show: false
+		show: false,
+		verticalAlignment: 'center'
+	},
+	computed: {
+		visibility: function() {
+			return this.get('show') ? 'visible' : 'hidden';
+		},
+		top: function() {
+			if (this.get('show')) {
+				if (this.get('verticalAlignment') === 'center') {
+					// calculate the height of the modal and screen to position it in the center
+					console.info('calc top');
+				}
+			}
+			else {
+				return -100;
+			}
+		}
+	},
+	onrender: function() {
+		// register the triggers
+		var triggers = this.get('triggers');
+		if (triggers) {
+			// $(triggers.join(',')).on('click', function() {
+			// 	this.set('show', true);
+			// }.bind(this));
+			console.info('');
+		}
 	}
 });
