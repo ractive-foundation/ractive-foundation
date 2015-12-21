4ba509f316cebc498124c745184daf8599f7c1e0
Nathan Flew <nath@nathf.com>
2015-12-22 08:51:39 +1100

    #288 Add two new options to cater for new scenarios.

--- tasks/seleniumServer.js
+++ tasks/seleniumServer.js
@@ -24,6 +24,11 @@ module.exports = function (options) {
 	var pingSelenium = function(options) {
 		options = options || {};
 		return Q.Promise(function (resolve, reject) {
+
+			if (options.seleniumOptions && options.seleniumOptions.alwaysSpawn) {
+				return resolve(false);
+			}
+
 			gutil.log(gutil.colors.gray('Checking if Selenium server is running'));
 
 			var opts = {
@@ -79,6 +84,11 @@ module.exports = function (options) {
 			if (!seleniumServer) {
 				gutil.log(gutil.colors.red('Cannot kill standalone server.'));
 				return reject('Cannot kill standalone server.');
+			}
+
+			if (options.seleniumOptions.kill === false) {
+				gutil.log('Finished', '\'' + gutil.colors.cyan('selenium standalone server') + '\'...');
+				return reject('Configured not to kill selenium.')
 			}
 
 			seleniumServer.kill();
