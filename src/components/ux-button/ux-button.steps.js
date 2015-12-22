a49e93ce64b06f6545db4a116323d3784eb99e09
Nathan Flew <nath@nathf.com>
2015-12-22 16:01:38 +1100

    Remove redundant steps

--- src/components/ux-button/ux-button.steps.js
+++ src/components/ux-button/ux-button.steps.js
@@ -15,37 +15,4 @@ module.exports = function () {
 
 	});
 
-	this.Then(/^the element "([^"]*)" should be displayed$/, function (semanticName, callback) {
-
-		var self = this;
-		this.client.isExisting(this.component[semanticName], function (err, isExisting) {
-			try {
-				self.assert(isExisting);
-				callback();
-			} catch (e) {
-				callback.fail('Assertion failed, element not found');
-			}
-		});
-	});
-
-	this.Then(/^the element "([^"]*)" should have the "([^"]*)" of "([^"]*)"$/,
-		function (semanticName, attribName, attribValue, callback) {
-
-			var self = this;
-
-			this.client.getAttribute(
-				this.component[semanticName].selector,
-				this.component[semanticName + 'Attribs'][attribName],
-				function (err, attr) {
-					try {
-						console.log(self);
-						self.assert.deepEqual(attr, attribValue);
-						callback();
-					} catch (e) {
-						callback.fail(e);
-					}
-				}
-			);
-		}
-	);
 };
