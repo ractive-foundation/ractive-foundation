6e7f7cd2bbf69c14494b5309f470cd01b1cb9c78
Eric Neo <eric.neo@optus.com.au>
2016-01-11 16:55:34 +1100

    242 adding test cases for pricing table

--- src/components/ux-pricingtable/ux-pricingtable.steps.js
+++ src/components/ux-pricingtable/ux-pricingtable.steps.js
@@ -12,6 +12,11 @@ module.exports = function () {
 		this.component = {};
 		this.component.container = '.ux-pricingtable ';
 		this.component.pricing_table = this.component.container + ' .pricing-table';
+		this.component.title = this.component.pricing_table + ' .title';
+		this.component.price = this.component.pricing_table + ' .price';
+		this.component.description = this.component.pricing_table + ' .description';
+		this.component.button = this.component.pricing_table + ' .cta-button';
+		this.component.bullet = this.component.pricing_table + ' .bullet-item';
 
 		callback();
 
