59731f3d391281ba188fcfcd34dc33d97be94863
Nathan Flew <nath@nathf.com>
2016-02-11 11:08:20 +1100

    #320 Add regex as options

--- tasks/rfCucumber.js
+++ tasks/rfCucumber.js
@@ -16,6 +16,8 @@ module.exports = function (options) {
 	var tags = [];
 	var files = [];
 	var runOptions = ['-f', format];
+	var stepRegex = options.stepRegex || '^($1)[^-_]';
+	var stepRegexFlags = options.stepRegexFlags || 'gi';
 
 	if (options.support) {
 		files = files.concat(glob([].concat(options.support)));
@@ -55,7 +57,7 @@ module.exports = function (options) {
 			// Match step name with feature name, stop searching when we hit - or _
 			// after first part match. In case we are looking for ux-list and we
 			// come across ux-list-item.
-			var regex = new RegExp('^(' + feature.name + ')[^-_]', 'gi');
+			var regex = new RegExp(stepRegex.replace('$1', feature.name), stepRegexFlags);
 			return parsedStepPath.name.match(regex);
 		});
 
