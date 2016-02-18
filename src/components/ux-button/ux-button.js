d9bb062b26ab03ab336531da4f2cde4933037f84
Nathan Flew <nath@nathf.com>
2016-02-19 08:42:50 +1100

    Remove event function and use proxy event, per documentation

--- src/components/ux-button/ux-button.js
+++ src/components/ux-button/ux-button.js
@@ -5,16 +5,5 @@ Ractive.extend({
 		return {
 			type: 'button'
 		};
-	},
-	clickHandler: function () {
-
-		// if a click event is specified propagate the click event
-		if (this.get('onclick')) {
-			console.log('Firing event');
-			this.fire(this.get('onclick'), this);
-		}
-
-		// prevent bubbling
-		return true;
 	}
 });
