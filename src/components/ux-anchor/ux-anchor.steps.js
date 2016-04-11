88b5637cd8a40782a6daec9178478f24a146300b
Ivan Wills <ivan.wills@gmail.com>
2016-04-11 18:14:41 +1000

    Removed old helpers

--- src/components/ux-anchor/ux-anchor.steps.js
+++ src/components/ux-anchor/ux-anchor.steps.js
@@ -12,22 +12,4 @@ module.exports = function () {
 		callback();
 	});
 
-	this.Given(/^I have loaded component "([^"]*)" with use case "([^"]*)"$/,
-		function (componentName, useCase, callback) {
-			this.client.loadComponentWithUseCase(componentName, useCase).then(function () {
-				callback();
-			});
-	});
-
-	this.Then(/^I should see the "([^"]*)" element$/, function (element, callback) {
-		this.client.waitForExist(this.component[element], this.defaultTimeout, function (err, exist) {
-			if (!exist) {
-				callback(new Error('Selector "' + this.component[element] + '" not found'));
-				return;
-			}
-
-			callback(err);
-		});
-	});
-
 };
